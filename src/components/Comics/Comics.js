a3cb14e868518824aa668ed5d72136203fa2f8b1
Maxim <bekrinmks@gmail.com>
2022-11-19 12:48:53 +0300

    we stopped at the characters

--- src/components/Comics/Comics.js
+++ src/components/Comics/Comics.js
@@ -1,4 +1,4 @@
-import {API_URL, URL_COMICS, IMG_STANDART_XLARGE, IMG_NOT_AVAILABLE} from '../../constants/api.js';
+import {API_URL, URL_COMICS, IMG_STANDART_XLARGE, IMG_NOT_AVAILABLE, URL_CHARACTERS} from '../../constants/api.js';
 import {getDataApi} from '../../utils/getDataApi';
 import { ROOT_INDEX } from '../../constants/root.js';
 
@@ -13,12 +13,14 @@ class Comics {
 
         data.forEach( ({id, title, thumbnail: {extension, path}}) => {
 
+
             if( !path.includes(IMG_NOT_AVAILABLE) ) {
 
+                const uri = API_URL + URL_COMICS +'/' + id +'/' + URL_CHARACTERS;
                 const imgSrc = path + '/' + IMG_STANDART_XLARGE + '.' + extension;
 
                 htmlContent += `
-                <li class="comics__item">
+                <li class="comics__item" data-uri="${uri}">
                     <span class="comics__name">${title}</span>
                     <img class="comics__img" src ="${imgSrc}" />
                 </li>
@@ -34,6 +36,17 @@ class Comics {
         `;
 
         ROOT_INDEX.innerHTML = htmlWrapper;
+    }
+
+    eventListener() {
+        document.querySelectorAll('.comics__item').forEach(element => {
+
+            const uri = element.getAttribute('data-uri')
+
+            element.addEventListener('click', function () {
+                console.log(uri);
+            });
+        });
     }
 }
 
